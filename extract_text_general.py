035eadd7cf231419b17f1469a861625788d4f133
sanchit <sanchitsh0211@gmail.com>
2019-08-16 19:26:24 +1000

    Updates to script

--- extract_text_general.py
+++ extract_text_general.py
@@ -5,9 +5,14 @@ from urllib.request import urlopen as U
 import requests
 import html2text
 import re
+import imgkit
+from PIL import Image
+import pytesseract
+from pytesseract import image_to_string
+
 
 # Load html content of url and store it in the soup variable 
-url = 'http://www.applebits.net/catalog/product_info.php?products_id=933'
+url = 'https://www.amtelectronics.net.au/product/apple-ipad-128gb-wi-fi-gold-6th-gen/'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 response = requests.get(url, headers=headers)
 content = response.content
@@ -32,43 +37,25 @@ for hidden in soup.body.find_all(style=re.compile(r'display:\s*none')):
 [x.extract() for x in soup.find_all('nav')]
 [x.extract() for x in soup.find_all('a')]
 [x.extract() for x in soup.find_all('footer')]
+[x.extract() for x in soup.find_all('iframe')]
+
+for div in soup.find_all("div", {'class':'footer'}): 
+    div.extract()
+
+for div in soup.find_all("div", {'class':'header'}): 
+    div.extract()
+
 comments = soup.findAll(text=lambda text: isinstance(text, Comment))
 [comment.extract() for comment in comments]
 
-# Convert remaining html content to text
-text = soup.get_text()
+for div in  soup.findAll("div", {"aria-hidden" : "true"}):
+    div.extract()
 
-# Break text into lines and remove leading and trailing space on each
-lines = (line.strip() for line in text.splitlines())
+with open('test.html', 'w') as f:
+    f.write(str(soup))
 
-# Break multi-headlines into a line each
-chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+imgkit.from_file('test.html', 'test.png')
 
-# Remove all blank lines
-text = '\n'.join(chunk for chunk in chunks if chunk)
-
-# Specify file name
-filename = "Book1.txt"
-
-# Write text to file
-with open(filename, 'w', encoding="utf-8") as f:
-    f.write(text)
-
-# Read file and store contents in set while preserving order 
-content = open(filename, 'r', encoding="utf-8")
-lines = content.readlines()
-texts = sorted(set(lines), key=lines.index)
-
-# Remove strings whose length is less than or equal to 3 from set
-for strings in texts:
-    if len(strings) <= 3:
-        texts.remove(strings)
-
-# Remove lines which contain only special characters and contain copyright symbol
-# Write filtered out text to file  
-with open(filename, 'w', encoding="utf-8") as fil:
-    for line in texts:
-        if not re.match(r'^[_\W]+$', line):
-            if not re.match(r'[©®™]', line):
-                if not re.match(r'[Copyright © ]', line):
-                    fil.write(line)
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+img = Image.open("test.png")
+text = image_to_string(img)
