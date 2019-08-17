18d71bbc051d7e6201b37897b4288c9996727390
sanchit <sanchitsh0211@gmail.com>
2019-08-18 01:34:03 +1000

    Updates to extraction script. Unicode issues to be resolved

--- extract_text_general.py
+++ extract_text_general.py
@@ -10,9 +10,8 @@ from PIL import Image
 import pytesseract
 from pytesseract import image_to_string
 
-
 # Load html content of url and store it in the soup variable 
-url = 'https://www.amtelectronics.net.au/product/apple-ipad-128gb-wi-fi-gold-6th-gen/'
+url = 'https://shop.workventures.com.au/product/toshiba-portege-z930/'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 response = requests.get(url, headers=headers)
 content = response.content
@@ -24,6 +23,14 @@ for hidden in soup.body.find_all(style=re.compile(r'display:\s*none')):
 
 for headless in (li for li in soup.find_all('li') if li.find('a')):
     headless.decompose()
+
+for label in soup.find_all('label'):
+    if label.find_next_siblings('input'):
+        label.decompose()
+
+for label in soup.find_all('label'):
+    if label.find_next_siblings('textarea'):
+        label.decompose()
 
 # Remove elements which generate noise in web page
 [x.extract() for x in soup.find_all('script')]
@@ -41,6 +48,7 @@ for headless in (li for li in soup.find_all('li') if li.find('a')):
 [x.extract() for x in soup.find_all('a')]
 [x.extract() for x in soup.find_all('footer')]
 [x.extract() for x in soup.find_all('iframe')]
+[x.extract() for x in soup.find_all('svg')]
 
 for div in soup.find_all("div", {'class':'footer'}): 
     div.extract()
@@ -48,11 +56,32 @@ for div in soup.find_all("div", {'class':'footer'}):
 for div in soup.find_all("div", {'class':'header'}): 
     div.extract()
 
+for elem in soup.find_all():
+    if elem.has_attr('aria-hidden'):
+        if elem['aria-hidden'] == 'true':
+            elem.extract()
+
+for elem in soup.find_all():
+    if elem.has_attr('type'):
+        if elem['type'] == 'hidden':
+            elem.extract()
+
 comments = soup.findAll(text=lambda text: isinstance(text, Comment))
 [comment.extract() for comment in comments]
 
-for div in  soup.findAll("div", {"aria-hidden" : "true"}):
-    div.extract()
+flag = True
+while(flag):
+    count = 0
+    for tags in soup.findAll():
+        inner_text = tags.text
+        content = inner_text.strip()
+        if len(content) == 0:
+            count = count+1
+            tags.extract()
+    if(count > 0):
+        flag = True
+    else:
+        flag = False
 
 with open('test.html', 'w') as f:
     f.write(str(soup))
@@ -66,26 +95,26 @@ text = image_to_string(img)
 # Specify file name
 filename = "test.txt"
 
+# Write text to file
 with open(filename, 'w') as f:
     f.write(text)
 
-# Write text to file
-
-#Read file and store contents in set while preserving order 
-# content = open(filename, 'r', encoding="utf-8")
-# lines = content.readlines()
-# texts = sorted(set(lines), key=lines.index)
+# Read file and store contents in set while preserving order 
+content = open(filename, 'r')
+lines = content.readlines()
+texts = sorted(set(lines), key=lines.index)
 
 # Remove strings whose length is less than or equal to 3 from set
-# for strings in texts:
-#     if len(strings) <= 3:
-#         texts.remove(strings)
+for strings in texts:
+    if len(strings) <= 3:
+        texts.remove(strings)
 
 # Remove lines which contain only special characters and contain copyright symbol
 # Write filtered out text to file  
-# with open(filename, 'w', encoding="utf-8") as fil:
-#     for line in texts:
-#         if not re.match(r'^[_\W]+$', line):
-#             if not re.match(r'[©®™]', line):
-#                 if not re.match(r'[Copyright © ]', line):
-#                     fil.write(line)
+with open(filename, 'w', encoding='utf-8') as fil:
+    for line in texts:
+        if not re.match(r'^[_\W]+$', line):
+            if not re.match(r'[©®™]', line):
+                if not re.match(r'[Copyright © ]', line):
+                    fil.write(line)
+
