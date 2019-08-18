6633a3eafd35092f4241a8fe3fc85e646e5164d9
sanchit <sanchitsh0211@gmail.com>
2019-08-19 01:12:31 +1000

    Updates to text extraction script

--- extract_text_general.py
+++ extract_text_general.py
@@ -9,112 +9,162 @@ import imgkit
 from PIL import Image
 import pytesseract
 from pytesseract import image_to_string
+from selenium import webdriver
+import os
+os.environ["LANG"] = "en_US.UTF-8"
 
 # Load html content of url and store it in the soup variable 
-url = 'https://shop.workventures.com.au/product/toshiba-portege-z930/'
+url = 'https://shop.workventures.com.au/product/lenovo-thinkpad-helix-2-in-1-laptop-tablet/'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 response = requests.get(url, headers=headers)
 content = response.content
 soup = BeautifulSoup(content)
 
+driver = webdriver.Chrome("C:/Users/sanch/Downloads/chromedriver_win32/chromedriver.exe")
+driver.get(url)
+
+font_sizes = []
+elems = driver.find_elements_by_xpath("//*[contains(text(), '$')]")
+for elem in elems:
+    if elem.tag_name != 'script' or elem.tag_name != 'style':
+        font_sizes.append(elem.value_of_css_property('font-size'))
+
+fonts = []
+for font in font_sizes:
+    fonts.append(font.replace('px',''))
+
+font_digits = []
+for size in fonts:
+    font_digits.append(float(size))
+
+max_font = max(font_digits) 
+max_font = int(max_font)
+max_font = str(max_font)
+
+for elem in elems:
+    if elem.tag_name != 'script' or elem.tag_name != 'style':
+        if max_font not in elem.value_of_css_property('font-size'):
+            print('Not Max')
+        else:
+            print('Max')
+
 # Remove all hidden elements from html content 
-for hidden in soup.body.find_all(style=re.compile(r'display:\s*none')):
-    hidden.decompose()
+# for hidden in soup.body.find_all(style=re.compile(r'display:\s*none')):
+#     hidden.decompose()
 
-for headless in (li for li in soup.find_all('li') if li.find('a')):
-    headless.decompose()
+# for headless in (li for li in soup.find_all('li') if li.find('a')):
+#     headless.decompose()
 
-for label in soup.find_all('label'):
-    if label.find_next_siblings('input'):
-        label.decompose()
+# tags = ['li', 'ul', 'ol']
+# for tag in tags: 
+#     for match in soup.findAll(tag):
+#         match.replaceWithChildren()
 
-for label in soup.find_all('label'):
-    if label.find_next_siblings('textarea'):
-        label.decompose()
+# for label in soup.find_all('label'):
+#     if label.find_next_siblings('input'):
+#         label.decompose()
+
+# for label in soup.find_all('label'):
+#     if label.find_next_siblings('textarea'):
+#         label.decompose()
 
 # Remove elements which generate noise in web page
-[x.extract() for x in soup.find_all('script')]
-[x.extract() for x in soup.find_all('style')]
-[x.extract() for x in soup.find_all('head')]
-[x.extract() for x in soup.find_all('title')]
-[x.extract() for x in soup.find_all('meta')]
-[x.extract() for x in soup.find_all('noscript')]
-[x.extract() for x in soup.find_all('select')]
-[x.extract() for x in soup.find_all('img')]
-[x.extract() for x in soup.find_all('textarea')]
-[x.extract() for x in soup.find_all('input')]
-[x.extract() for x in soup.find_all('button')]
-[x.extract() for x in soup.find_all('nav')]
-[x.extract() for x in soup.find_all('a')]
-[x.extract() for x in soup.find_all('footer')]
-[x.extract() for x in soup.find_all('iframe')]
-[x.extract() for x in soup.find_all('svg')]
+[x.decompose() for x in soup.find_all('script')]
 
-for div in soup.find_all("div", {'class':'footer'}): 
-    div.extract()
+[x.decompose() for x in soup.find_all('head')]
+[x.decompose() for x in soup.find_all('title')]
+[x.decompose() for x in soup.find_all('meta')]
+[x.decompose() for x in soup.find_all('noscript')]
+[x.decompose() for x in soup.find_all('select')]
+[x.decompose() for x in soup.find_all('img')]
+[x.decompose() for x in soup.find_all('textarea')]
+[x.decompose() for x in soup.find_all('input')]
+[x.decompose() for x in soup.find_all('button')]
+[x.decompose() for x in soup.find_all('nav')]
+[x.decompose() for x in soup.find_all('a')]
+[x.decompose() for x in soup.find_all('footer')]
+[x.decompose() for x in soup.find_all('iframe')]
+[x.decompose() for x in soup.find_all('svg')]
+[x.decompose() for x in soup.find_all('aside')]
+[x.decompose() for x in soup.find_all('header')]
 
-for div in soup.find_all("div", {'class':'header'}): 
-    div.extract()
+# [x.decompose() for x in soup.find_all('style')]
 
-for elem in soup.find_all():
-    if elem.has_attr('aria-hidden'):
-        if elem['aria-hidden'] == 'true':
-            elem.extract()
+# for div in soup.find_all("div", {'class':'footer'}): 
+#     div.decompose()
 
-for elem in soup.find_all():
-    if elem.has_attr('type'):
-        if elem['type'] == 'hidden':
-            elem.extract()
+# for div in soup.find_all("div", {'class':'header'}): 
+#     div.decompose()
 
-comments = soup.findAll(text=lambda text: isinstance(text, Comment))
-[comment.extract() for comment in comments]
+# comments = soup.findAll(text=lambda text: isinstance(text, Comment))
+# [comment.extract() for comment in comments]
 
-flag = True
-while(flag):
-    count = 0
-    for tags in soup.findAll():
-        inner_text = tags.text
-        content = inner_text.strip()
-        if len(content) == 0:
-            count = count+1
-            tags.extract()
-    if(count > 0):
-        flag = True
-    else:
-        flag = False
+# for tags in soup.findAll():
+#         inner_text = tags.text
+#         content = inner_text.strip()
+#         if len(content) <= 3:
+#             tags.extract()
 
-with open('test.html', 'w') as f:
-    f.write(str(soup))
+# for tags in soup.findAll():
+#         inner_text = tags.text
+#         content = inner_text.strip()
+#         if re.match(r'[©®™]', content):
+#             tags.extract()
 
-imgkit.from_file('test.html', 'test.png')
+# for tags in soup.findAll():
+#         inner_text = tags.text
+#         content = inner_text.strip()
+#         if re.match(r'[Copyright © ]', content):
+#             tags.extract()
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-img = Image.open("test.png")
-text = image_to_string(img)
+# flag = True
+# while(flag):
+#     count = 0
+#     for tags in soup.findAll():
+#         inner_text = tags.text
+#         content = inner_text.strip()
+#         if len(content) == 0:
+#             count = count+1
+#             tags.extract()
+#     if(count > 0):
+#         flag = True
+#     else:
+#         flag = False
 
-# Specify file name
-filename = "test.txt"
 
-# Write text to file
-with open(filename, 'w') as f:
-    f.write(text)
+            
+# with open('test.html', 'w', encoding='utf-8-sig') as f:
+#     f.write(str(soup))
 
-# Read file and store contents in set while preserving order 
-content = open(filename, 'r')
-lines = content.readlines()
-texts = sorted(set(lines), key=lines.index)
+# imgkit.from_file('test.html', 'test.png')
 
-# Remove strings whose length is less than or equal to 3 from set
-for strings in texts:
-    if len(strings) <= 3:
-        texts.remove(strings)
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# img = Image.open("test.png")
+# text = image_to_string(img)     
 
-# Remove lines which contain only special characters and contain copyright symbol
-# Write filtered out text to file  
-with open(filename, 'w', encoding='utf-8') as fil:
-    for line in texts:
-        if not re.match(r'^[_\W]+$', line):
-            if not re.match(r'[©®™]', line):
-                if not re.match(r'[Copyright © ]', line):
-                    fil.write(line)
+# # Specify file name
+# filename = "test.txt"
+
+# # Write text to file
+# with open(filename, 'w') as f:
+#     f.write(text)
+
+# # Read file and store contents in set while preserving order 
+# content = open(filename, 'r')
+# lines = content.readlines()
+# texts = sorted(set(lines), key=lines.index)
+
+# # Remove strings whose length is less than or equal to 3 from set
+# for strings in texts:
+#     if len(strings) <= 3:
+#         texts.remove(strings)
+
+# # Remove lines which contain only special characters and contain copyright symbol
+# # Write filtered out text to file  
+# with open(filename, 'w') as fil:
+#     for line in texts:
+#         if not re.match(r'^[_\W]+$', line):
+#             if not re.match(r'[©®™]', line):
+#                 if not re.match(r'[Copyright © ]', line):
+#                     fil.write(line)
 
