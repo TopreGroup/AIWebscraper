ad1d3a0e3b62ec54a90d08330292486a93e267cc
Sanchit0011 <sanchitsh0211@gmail.com>
2019-09-18 17:03:18 +1000

    Update store_productURL.py

--- store_productURL.py
+++ store_productURL.py
@@ -1,7 +1,12 @@
 """ Import the necessary modules """
 import pyodbc
 
-id = 0 
+id = 0
+
+business_id = 0
+
+""" Specify the inputted business name """
+business_name = 'Reboot-IT'
 
 """ Establish connection to microsoft sql server """
 conn = conn = pyodbc.connect('Driver={SQL Server};'
@@ -28,12 +33,16 @@ for fl in file_lines:
 """ Converting working product urls list to set """
 urls = sorted(set(urls), key=urls.index)
 
+""" Retrieve business id for the inputted business name """
+cur.execute("SELECT business_id from BUSINESSES WHERE CONVERT(varchar, business_name) = '"+ business_name + "';")
+business_id = cur.fetchone()[0]
+
 """ Loop through all working product urls and store them in the database """
 for url in urls:
     id = id + 1
-    postgres_insert_query = """SET ANSI_WARNINGS OFF; INSERT INTO ENTITIES (entity_id, brand, model, price, stock, producturl, condition) \
-VALUES (?, ?, ?, ?, ?, ?, ?); SET ANSI_WARNINGS ON; """
-    record_to_insert = (str(id), '', '', '', '', url, '')
+    postgres_insert_query = """SET ANSI_WARNINGS OFF; INSERT INTO ENTITIES (entity_id, brand, model, price, stock, producturl, condition, business_id) \
+VALUES (?, ?, ?, ?, ?, ?, ?, ?); SET ANSI_WARNINGS ON; """
+    record_to_insert = (str(id), '', '', '', '', url, '', business_id)
     cur.execute(postgres_insert_query, record_to_insert)
     conn.commit()
 
