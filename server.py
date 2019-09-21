0795bc4f397e922fb9d4447baae7d564d01437e9
Sanchit0011 <sanchitsh0211@gmail.com>
2019-09-21 19:55:28 +1000

    Update server.py

--- server.py
+++ server.py
@@ -21,17 +21,17 @@ def trance():
             print(uiobjs)
             for uiobj in uiobjs:
                 if(uiobj["bname"] and uiobj["burl"] and uiobj["btitle"]):
-                    parsed_uri = urlparse(uiobj["burl"])
-                    bdomain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-                    id.flowStart(uiobj["bname"], uiobj["burl"], uiobj["btitle"], bdomain)
                     conn = pyodbc.connect('Driver={SQL Server};''Server=DESKTOP-1P8QTPD;''Database=CRFTest;''UID=Sanchit12;''PWD=GSWarrior02;''Trusted_Connection=yes;')
                     cur = conn.cursor()
                     postgres_insert_query = """SET ANSI_WARNINGS OFF; INSERT INTO BUSINESSES (business_name, business_url) \
                     VALUES (?, ?); SET ANSI_WARNINGS ON; """
-                    record_to_insert = (uiobj["bname"], uiobj["burl"])
+                    parsed_uri = urlparse(uiobj["burl"])
+                    bdomain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+                    record_to_insert = (uiobj["bname"], bdomain)
                     cur.execute(postgres_insert_query, record_to_insert)
                     conn.commit()
                     conn.close()
+                    id.flowStart(uiobj["bname"], uiobj["burl"], uiobj["btitle"], bdomain)
                     render_template('index.html', notify="success")
                 else:
                     render_template('index.html', notify="error")
