cab5f77c0a24a2a5ac0e113e645e8011c3b69de3
Sanchit0011 <sanchitsh0211@gmail.com>
2019-10-16 11:56:57 +1100

    Update store_productURL.py

--- store_productURL.py
+++ store_productURL.py
@@ -1,35 +1,30 @@
 """ Import the necessary modules """
 import pytds
+import subprocess
+import glob
+import os
 from google.cloud import storage
 
 def storeProdURL(name):
-    id = 0
 
     business_id = 0
 
     """ Specify the inputted business name """
     business_name = name
-
-    client = storage.Client()
     
-    bucket_name = "trunkedfilestorage"
-    
-    bucket = client.get_bucket(bucket_name)
-
     """ Establish connection to microsoft sql server """
     conn = conn = pytds.connect('devdb.trunked.com.au', 'trunkedproject', 'trunkedproject', 'rmitProject@trunked')
 
     cur = conn.cursor()
-
-    """ Reading lines from working product urls text file """  
-    file_lines = []
-    blob = bucket.get_blob('producturl.txt')
-    out_file = open("producturl.txt", 'wb')
-    file_content = blob.download_as_string()
-    out_file.write(file_content)
-    out_file.close()
     
-    with open("producturl.txt", "r") as fs:
+    bucket_name = "crfmodel"
+    client = storage.Client()
+    bucket = client.get_bucket(bucket_name)
+
+    """ Reading lines from working product urls text file """
+    file_lines = []
+    
+    with open("working_product_urls.txt", "r") as fs:
         for line in fs:
             currentLine = line.rstrip().split(',')
             file_lines.append(currentLine)
@@ -38,7 +33,7 @@ def storeProdURL(name):
     urls = []
     for fl in file_lines:
         urls.append(fl[0])
-	
+
     """ Converting working product urls list to set """
     urls = sorted(set(urls), key=urls.index)
 
@@ -46,16 +41,28 @@ def storeProdURL(name):
     cur.execute("SELECT business_id from BUSINESSES WHERE CONVERT(varchar, business_name) = " + "'" + business_name + "'" + ";")
     business_id = cur.fetchone()[0]
 
-
-    
     """ Loop through all working product urls and store them in the database """
     for url in urls:
-        id = id + 1
-        postgres_insert_query = """SET ANSI_WARNINGS OFF; INSERT INTO ENTITIES (entity_id, brand, model, price, stock, producturl, condition, business_id, category) \
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s); SET ANSI_WARNINGS ON; """
-        record_to_insert = (str(id), '', '', '', '', url, '', business_id, '')
+        postgres_insert_query = """SET ANSI_WARNINGS OFF; INSERT INTO ENTITIES (brand, model, price, stock, producturl, condition, category, business_id) \
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s); SET ANSI_WARNINGS ON; """
+        record_to_insert = ('', '', '', '', url, '', '', business_id)
         cur.execute(postgres_insert_query, record_to_insert)
         conn.commit()
 
     """ Close the connection """
     conn.close()
+    
+    blob = bucket.blob("ner-model.ser.gz")
+    blob.download_to_filename("ner-model.ser.gz")
+    
+    subprocess.call(['/opt/java-jdk/jdk1.8.0_221/bin/java', '-jar', '/home/sanchitsh0211/AIWebscraper/crf.jar', "/home/sanchitsh0211/ner-model.ser.gz", "/home/sanchitsh0211/working_product_urls.txt"])
+    
+    files = glob.glob('./extracted_data/*')
+    for f in files:
+        os.remove(f)
+    
+    files = glob.glob('./tagged_data/*')
+    for f in files:
+        os.remove(f)
+    
+    print("flow complete.")
