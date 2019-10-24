2f32ff08554094f971c47451959841eead9f2ab5
Sanchit0011 <sanchitsh0211@gmail.com>
2019-10-24 18:42:32 +1100

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+chmod +x /home/adamkostarelas/AIProductExtraction/server.py
+dos2unix /home/adamkostarelas/AIProductExtraction/server.py
+sudo /home/adamkostarelas/AIProductExtraction/server.py
